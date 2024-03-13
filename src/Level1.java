e383b66803d2466ac5d2d90bfbbd746079056511
MajesticNthg <majestic_nthg@mail.ru>
2024-03-13 20:18:18 +1000

    mod.3

--- src/Level1.java
+++ src/Level1.java
@@ -1,21 +1,23 @@
 public class Level1 {
 
-    public static String PatternUnlock(int N, int[] hits) {
+    public static String PatternUnlock(int N, int [] hits)  {
         double sum = 0;
 
+
         for (int x = 0; x < hits.length - 1; x++) {
-            if (hits[x + 1] - hits[x] == 1)
+            if (hits[x+ 1] - hits[x] == 1)
                 sum += 1;
-            else if (hits[x] + hits[x + 1] == 8 || hits[x] + hits[x + 1] == 6 || hits[x] + hits[x + 1] == 11 || hits[x] + hits[x + 1] == 9)
+            else
+            if (hits[x] + hits[x + 1] == 8 || hits[x] + hits[x + 1] == 6 || hits[x] + hits[x + 1] == 11 || hits[x] + hits[x + 1] == 9)
                 sum += 1.414213;
             else
                 sum += 1;
         }
 
 
-        String s = String.valueOf(sum);
+        String s = String.valueOf(sum*100000);
         char digit = s.charAt(7);
-        if (digit > 5)
+        if (digit > '5')
             sum += 0.00001;
 
 
@@ -24,8 +26,8 @@ public class Level1 {
         int sum2 = 0;
         int des = 1;
 
-        for (int i = 0; sum > 0; i++, sum /= 10) {
-            f = (int) (sum % 10);
+        for (int i = 0; sum > 1; i++, sum /= 10) {
+            f = (int)(sum % 10);
             if (f != 0) {
                 sum2 += f * des;
                 des *= 10;
@@ -38,8 +40,10 @@ public class Level1 {
         return result;
 
     }
-
+    public static void main(String[] args) {
+        int[] hits = new int[] {1, 2, 3, 4, 5, 6, 2, 7, 8, 9};
+        int N = 10;
+        System.out.println(PatternUnlock(N, hits));
+    }
 
 }
-
-
